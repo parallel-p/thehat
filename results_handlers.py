0aa1b6ceb7fc4ded7141051826aaaaf993760f06
Kozlovtsev Konstantin <kozlovtsev179@gmail.com>
2013-12-31 10:53:36 +0400

    fix results

--- results_handlers.py
+++ results_handlers.py
@@ -1,9 +1,9 @@
 import webapp2
 
 class PostResultsHandler(webapp2.RequestHandler):
-    def post(self, id):
-        self.response.write('I\'ll give results to device from game with id №' + id)
+    def post(self, game_id):
+        self.response.write('I\'ll give results to device from game with id ' + str(game_id))
 
 class GetResultsHandler(webapp2.RequestHandler):
-    def get(self, id):
-        self.response.write('I\'ll get results from game with id №' + id)
+    def get(self, game_id):
+        self.response.write('I\'ll get results from game with id ' + str(game_id))
