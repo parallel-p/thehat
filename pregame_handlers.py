8068729f79133fee98aa400e973ce122e42b96d4
Nikolay Kalinin <nakalinin@gmail.com>
2013-12-31 15:59:47 +0400

    Make PreGame handlers inherited from AllHandler

--- pregame_handlers.py
+++ pregame_handlers.py
@@ -1,47 +1,51 @@
 __author__ = 'nikolay'
 import webapp2
-
-class PreGameAllHandler(webapp2.RequestHandler):
-    def set_device_id(self, **kwargs):
-        self.device_id = kwargs.get('device_id', None)
+from all_handler import AllHandler
 
 
-class PreGameCreateHandler(PreGameAllHandler):
+class PreGameCreateHandler(AllHandler):
     def post(self, **kwargs):
         super(PreGameCreateHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll create new pregame! device_id = %s' % self.device_id)
 
 
-class PreGameHandler(webapp2.RequestHandler):
+class PreGameHandler(AllHandler):
     def get(self, **kwargs):
+        super(PreGameHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll give pregame!')
 
 
-class PreGameUpdateHandler(webapp2.RequestHandler):
+class PreGameUpdateHandler(AllHandler):
     def post(self, **kwargs):
+        super(PreGameUpdateHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll update pregame!')
 
 
-class PreGameVersionHandler(webapp2.RequestHandler):
+class PreGameVersionHandler(AllHandler):
     def get(self, **kwargs):
+        super(PreGameVersionHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll give version of pregame!')
 
 
-class PreGameSinceHandler(webapp2.RequestHandler):
+class PreGameSinceHandler(AllHandler):
     def get(self, **kwargs):
+        super(PreGameSinceHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll give diff of pregame since some version!')
 
 
-class PreGameStartHandler(webapp2.RequestHandler):
+class PreGameStartHandler(AllHandler):
     def post(self, **kwargs):
+        super(PreGameStartHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll start pregame !')
 
 
-class PreGameAbortHandler(webapp2.RequestHandler):
+class PreGameAbortHandler(AllHandler):
     def post(self, **kwargs):
+        super(PreGameAbortHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll abort pregame !')
 
 
-class PreGameJoinHandler(webapp2.RequestHandler):
+class PreGameJoinHandler(AllHandler):
     def post(self, **kwargs):
+        super(PreGameJoinHandler, self).set_device_id(**kwargs)
         self.response.write('I\'ll join some pregame!')
