44194629113c8528364d7807092726672532f86c
Kozlovtsev Konstantin <kozlovtsev179@gmail.com>
2013-12-31 09:54:04 +0400

    add results handlers

---
+++ results_handlers.py
@@ -0,0 +1,9 @@
+import webapp2
+
+class PostResultsHandler(webapp2.RequestHandler):
+    def post(self):
+        pass
+
+class GetResultsHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
