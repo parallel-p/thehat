6573f632e6beddd4e7f09cb00f96dbd019a6dc0a
Ivan Pavlov <i.pavlov17@gmail.com>
2014-01-02 09:35:45 +0400

    fixed db/ndv

--- complain_word_handlers.py
+++ complain_word_handlers.py
@@ -11,16 +11,17 @@ class ComplainWordHandler(AllHandler):
         super(ComplainWordHandler, self).set_device_id(**kwargs)
         complained_word_json_list = \
             json.loads(self.request.get(r'complained_words'))
-        for current_complained_word_json in complained_word_json_list:
-            current_complained_word = ComplainedWord()
-            current_complained_word.word = current_complained_word_json['word']
-            current_complained_word.cause = \
-                int(current_complained_word_json['cause'])
-            current_complained_word.device_id = self.device_id
-            if 'replace_word' in current_complained_word_json:
-                current_complained_word.replacement_word = \
-                    current_complained_word_json['replace_word']
-            current_complained_word.put()
+
+        for current_word_json in complained_word_json_list:
+            current_word = ComplainedWord()
+            current_word.word = current_word_json['word']
+            current_word.cause = \
+                int(current_word_json['cause'])
+            current_word.device_id = self.device_id
+            if 'replace_word' in current_word_json:
+                current_word.replacement_word = \
+                    current_word_json['replace_word']
+            current_word.put()
         self.response.write('post, ok, device_id ='
                             '{0}\n'.format(self.device_id))
 
