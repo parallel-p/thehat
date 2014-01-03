492478523e315246a31cafdb9600612a6afb1cb2
Igor Labutin <Labutin.IgorL@gmail.com>
2014-01-03 17:36:40 +0400

    fixed dictionaries_packages_handlers and tests

--- dictionaries_packages_handlers.py
+++ dictionaries_packages_handlers.py
@@ -1,40 +1,72 @@
 import webapp2
 import json
+from all_handler import AllHandler
+from objects.user_devices import get_user_by_device
+from objects.user_streams import UserStreams
+from objects.dictionaries_packages import PackagesStream, PackageDictionary
 
 
-class GetStreamsListHandler(webapp2.RedirectHandler):
+class GetStreamsListHandler(AllHandler):
     def get(self, **kwargs):
-        streams_list = [{"id": "1", "name": "stream1"}, {"id": "2", "name": "stream2"}]
-
-        json_obj = {"streams": streams_list}
+        streams_list = PackagesStream.query()
+        json_obj = {'streams': []}
+        for stream in streams_list:
+            json_obj['streams'].append({'id': stream.id, 'name': stream.name})
 
         self.response.write(json.dumps(json_obj))
-        #self.response.write('I\'ll give list of streams!')
 
 
-class ChangeStreamStateHandler(webapp2.RedirectHandler):
+class ChangeStreamStateHandler(AllHandler):
     def post(self, **kwargs):
-        self.response.write('I\'ll change state of stream ' +
-                            kwargs.get('stream_id', "") + ' to ' +
-                            kwargs.get('on', "") + '!')
+        super(ChangeStreamStateHandler, self).set_device_id(**kwargs)
+        user_id = get_user_by_device(self.device_id)
+        subscribe_list = UserStreams.query(UserStreams.user_id == user_id).fetch(1)
+
+        if len(subscribe_list) == 0:
+            self.error(404)
+        else:
+            stream_id = kwargs.get('stream_id')
+            if kwargs.get('on', '') == 'true':
+                if stream_id not in subscribe_list[0].streams:
+                    subscribe_list[0].streams.append(stream_id)
+            else:
+                if stream_id in subscribe_list[0].streams:
+                    subscribe_list[0].streams.remove(stream_id)
 
 
-class GetPackagesListHandler(webapp2.RedirectHandler):
+class GetPackagesListHandler(AllHandler):
     def get(self, **kwargs):
-        packages_list = [{"id": "1", "name": "package1", "release_time": 1},
-                         {"id": "2", "name": "package2", "release_time": 2}]
+        super(GetPackagesListHandler, self).set_device_id(**kwargs)
+        packages_stream = PackagesStream.query(PackagesStream.id == kwargs.get('stream_id')).fetch(1)
 
-        json_obj = {"packages": packages_list}
+        if len(packages_stream) == 0:
+            self.error(404)
+        else:
+            packages_list = []
+            for package_id in packages_stream[0].packages_id_list:
+                packages = PackageDictionary.query(PackageDictionary.id == package_id).fetch(1)
+                if len(packages) == 0:
+                    self.error(404)
+                packages_list.append(packages[0])
+            json_obj = {'packages': []}
+            for package in packages_list:
+                json_obj['packages'].append(
+                    {'id': package.id, 'name': package.name, 'release_time': package.release_time})
 
-        self.response.write(json.dumps(json_obj))
-        #self.response.write('I\'ll give list of packages from stream '
-        #                    + kwargs.get('stream_id', None) + '!')
+            self.response.write(json.dumps(json_obj))
 
 
-class GetPackageHandler(webapp2.RedirectHandler):
+class GetPackageHandler(AllHandler):
     def get(self, **kwargs):
-        package1 = {"id": "1", "name": "package1", "release_time": 1, "words": ["tea", "coffee"]}
+        super(GetPackageHandler, self).set_device_id(**kwargs)
+        packages = PackageDictionary.query(PackageDictionary.id == kwargs.get('package_id')).fetch(1)
 
-        self.response.write(json.dumps(package1));
-        #self.response.write('I\'ll give package ' +
-        #                    kwargs.get('package_id', None) + '!')
+        if len(packages) == 0:
+            self.error(404)
+        else:
+            package = packages[0]
+
+            json_obj = {"id": package.id, "name": package.name, "release_time": package.release_time,
+                        "words": package.words}
+
+            self.response.write(json.dumps(json_obj))
