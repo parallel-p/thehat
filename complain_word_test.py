9071b0338925fe9761fdb296428ce954f03d1b7c
Nikolay Kalinin <nakalinin@gmail.com>
2014-01-03 15:54:49 +0400

    fixed complain_word_test

--- complain_word_test.py
+++ complain_word_test.py
@@ -17,9 +17,9 @@ class complain_word_test(unittest2.TestCase):
         self.testbed.init_memcache_stub()
 
     def test_post(self):
-        word1 = {"word": "vasya", "cause": "1",
+        word1 = {"word": "vasya", "cause": 1,
                  "replace_word": "petya"}
-        word2 = {"word": "vasya", "cause": "2"}
+        word2 = {"word": "vasya", "cause": 2}
         len_before = ComplainedWord.all().count()
         request = webapp2.Request.blank('/abc/complain')
         request.body = "complained_words={0}". \
