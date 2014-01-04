7bee609d3c57f29c4bfc099a147480675b145408
Kozlovtsev Konstantin <kozlovtsev179@gmail.com>
2014-01-04 19:00:01 +0400

    make results and now it works

--- log_n_res_handlers.py
+++ log_n_res_handlers.py
@@ -4,30 +4,32 @@ from google.appengine.ext import ndb
 #from log_classes import *
 from all_handler import AllHandler
 from objects.user_devices import get_user_by_device
-from datetime import datetime
+import time
 
 # word states: GUESSED, NOT_GUESSED, FAILED = range(3)
 
 
+def make_timestamp():
+    return int(1000*time.time())
+
+
 class Results(ndb.Model):
-    results_json = ndb.JsonProperty()
+    results_json = ndb.JsonProperty(indexed=True)
     players_ids = ndb.StringProperty(repeated=True)
     timestamp = ndb.IntegerProperty()
 
-    #def __init__(self, results_json, players_ids, **kwargs):
-    #    super(Results, self).__init__(kwargs)
-    #    self.results_json = results_json
-    #    self.players_ids = players_ids
-    #    self.timestamp = int(datetime.now().timestamp())
+
+class Log(ndb.Model):
+    json = ndb.JsonProperty()
 
 
 class UploadLog(AllHandler):
     def post(self, **kwargs):
         super(UploadLog, self).set_device_id(**kwargs)
         game_id = kwargs["game_id"]
-        log_json = ndb.JsonProperty(id=game_id)
-        log_json = self.request.get("log")
-        log_json.put()
+        log = Log(id=game_id)
+        log.json = self.request.get("log")
+        log.put()
 
 
 class UploadRes(AllHandler):
@@ -47,19 +49,18 @@ class UploadRes(AllHandler):
         results = Results(id=game_id)
         results.results_json = results_json
         results.players_ids = players_ids
+        results.timestamp = make_timestamp()
         results.put()
 
 
 class CheckAnyResults(AllHandler):
     def get(self, **kwargs):
+        super(CheckAnyResults, self).set_device_id(**kwargs)
         player_id = get_user_by_device(self.device_id)
         timestamp = kwargs["timestamp"]
         results = Results.query(Results.players_ids.IN([player_id]),
-                                Results.timestamp > timestamp).fetch(projection=["results_json"])
-        #if len(results) == 0:
-        #    self.error(401)  # have no access
-        #    return
-        response = [{'result': result.json, 'timestamp': datetime.now().timestamp()} for result in results]
+                                Results.timestamp > int(timestamp)).fetch(projection=["results_json"])
+        response = [{'result': result.results_json, 'timestamp': make_timestamp()} for result in results]
         self.response.write(json.dumps(response))
 
 
