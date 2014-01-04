c6f288779ecbe7f78805b25418316322623a01c7
Kozlovtsev Konstantin <kozlovtsev179@gmail.com>
2014-01-04 19:00:01 +0400

    make results and now it works

--- log_n_res_handlers_test.py
+++ log_n_res_handlers_test.py
@@ -12,11 +12,6 @@ GAME = '''{
             "id": 1,
             "name": "Vasya",
             "words": []
-        },
-        {
-            "id": 2,
-            "name": "Petya",
-            "words": []
         }
     ],
     "words": [],
@@ -45,21 +40,28 @@ class TestResults(unittest.TestCase):
         self.testbed.init_memcache_stub()
 
     def create_game(self):
-        request = webapp2.Request.blank('/device_id/pregame/create')
+        request = webapp2.Request.blank('/device_1/pregame/create')
         request.method = 'POST'
         request.body = "json=%s" % GAME
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
         json_returned = json.loads(response.body)
-        return json_returned['id']
+        return json_returned['id'], json_returned['pin']
 
-    def test_upload_n_load_results(self):
-        request = webapp2.Request.blank('/1/upload_results/%s' % URLSAFE)
+    def join(self, device_id, pin):
+        request = webapp2.Request.blank('/%s/pregame/join' % device_id)
         request.method = 'POST'
-        request.body = "results=%s" % SOME_RES
+        request.body = "json=%s" % str(pin)
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
-        request = webapp2.Request.blank('/1/get_results/%s' % URLSAFE)
+
+    def test_upload_n_load_results(self):
+        request = webapp2.Request.blank('/device_1/upload_results/%s' % URLSAFE)
+        request.method = 'POST'
+        request.body = 'results=%s' % SOME_RES
+        response = request.get_response(main.app)
+        self.assertEqual(response.status_int, 200)
+        request = webapp2.Request.blank('/device_1/get_results/%s' % URLSAFE)
         request.method = 'GET'
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
@@ -67,24 +69,27 @@ class TestResults(unittest.TestCase):
 
     def test_pregame_upload_n_check_res(self):
         # create 2 games:
-        game1_id = self.create_game()
-        game2_id = self.create_game()
+        game1_id, game1_pin = self.create_game()
+        game2_id, game2_pin = self.create_game()
+        self.join('device_2', game1_pin)
+        self.join('device_2', game2_pin)
 
         # check for res, return empty str:
-        request = webapp2.Request.blank('1/check_for_results/0')
+        request = webapp2.Request.blank('/device_1/check_for_results/0')
         request.method = 'GET'
         response = request.get_response(main.app)
-        self.assertEqual(response.body, '')
+        results = json.loads(response.body)
+        self.assertEqual(len(results), 0)
 
         # upload some res from game1:
-        request = webapp2.Request.blank('/1/upload_results/%s' % game1_id)
+        request = webapp2.Request.blank('/device_1/upload_results/%s' % game1_id)
         request.method = 'POST'
         request.body = "results=%s" % SOME_RES
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
 
         # check now - must return nothing but some res
-        request = webapp2.Request.blank('1/check_for_results/0')
+        request = webapp2.Request.blank('/device_1/check_for_results/0')
         request.method = 'GET'
         response = request.get_response(main.app)
         results = json.loads(response.body)
@@ -93,14 +98,14 @@ class TestResults(unittest.TestCase):
         timestamp = results[0]['timestamp']
 
         # we played second game so push res:
-        request = webapp2.Request.blank('/1/upload_results/%s' % game2_id)
+        request = webapp2.Request.blank('/device_1/upload_results/%s' % game2_id)
         request.method = 'POST'
         request.body = "results=%s" % SOME_RES
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
 
         # now player with id 2 aren't download results:
-        request = webapp2.Request.blank('2/check_for_results/0')
+        request = webapp2.Request.blank('/device_2/check_for_results/0')
         request.method = 'GET'
         response = request.get_response(main.app)
         results = json.loads(response.body)
@@ -109,7 +114,7 @@ class TestResults(unittest.TestCase):
         self.assertEqual(results[1]['result'], SOME_RES)
 
         # but player with id 1 already download res of game1:
-        request = webapp2.Request.blank('1/check_for_results/%s' % timestamp)
+        request = webapp2.Request.blank('/device_1/check_for_results/%s' % timestamp)
         request.method = 'GET'
         response = request.get_response(main.app)
         results = json.loads(response.body)
@@ -117,14 +122,14 @@ class TestResults(unittest.TestCase):
         self.assertEqual(results[0]['result'], SOME_RES)
 
         # and player with id 3 have no access to games:
-        request = webapp2.Request.blank('3/check_for_results/0')
+        request = webapp2.Request.blank('/device_3/check_for_results/0')
         request.method = 'GET'
         response = request.get_response(main.app)
         results = json.loads(response.body)
         self.assertEqual(len(results), 0)
 
     def test_upload_log(self):
-        request = webapp2.Request.blank('/1/upload_log/%s' % URLSAFE)
+        request = webapp2.Request.blank('/device_1/upload_log/%s' % URLSAFE)
         request.method = 'POST'
         request.body = "log=%s" % SOME_LOG
         response = request.get_response(main.app)
@@ -132,10 +137,10 @@ class TestResults(unittest.TestCase):
         ndb.Key('Log', URLSAFE)
 
     def test_load_non_existent_res(self):
-        request = webapp2.Request.blank('/1/get_results/%s' % URLSAFE)
+        request = webapp2.Request.blank('/device_1/get_results/%s' % URLSAFE)
         request.method = 'GET'
         response = request.get_response(main.app)
-        self.assertEqual(response.status_int, 404) #not found
+        self.assertEqual(response.status_int, 404)  # not found
 
     def TearDown(self):
         self.testbed.deactivate()
