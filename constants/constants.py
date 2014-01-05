47fb935df364b35261795ad4ad9800145e5fb060
Ivan Pavlov <i.pavlov17@gmail.com>
2014-01-05 17:52:51 +0400

    added admin panel

--- constants/constants.py
+++ constants/constants.py
@@ -13,8 +13,8 @@ get_title = "json"
 all_words_deleted_message = "All words deleted"
 deleted_word_name = "word"
 
-delete_all_url = "/TaSsP25Hb2sEqRCuAjxcbwXNjLR3Ole3lijbIUKcao1yxh2790sJDScHnVdxIVchyDTM32r0WWyF14gqxLbjEfLCWpCGjPJwizvZ"
+delete_all_url = "/delete_all_words"
 show_complained_url = "/complained"
-delete_current_url = "/cN08Yh3CNWaIVe0OAeBDflxw79hQP44CvEtGe4doGyVUgaNMgRKrazdGPcobgugxGAwV2Z2fP1vBD8sa5qO89ZVLX41x4t91XnFV"
-delete_from_global_url = "/pYDpUoXspsJTVhqBai7hrV7uKs5MN0VUhE80ksWGGuQhazGbVPUhcPSzpztdboY34yFstsL56GMHkOI3SWUtF5Cwml23NDt6Yuou"
+delete_current_url = "/delete_curr"
+delete_from_global_url = "/delete_glob"
 
