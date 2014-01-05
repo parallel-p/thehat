40bbf9e8fc7a73eefb0d5a4f8e919f0e9c22e5f2
Igor Labutin <Labutin.IgorL@gmail.com>
2014-01-05 16:33:24 +0400

    dictionaries_packages_admin_handlers and tests fixed

--- dictionaries_packages_admin_handlers_test.py
+++ dictionaries_packages_admin_handlers_test.py
@@ -15,24 +15,15 @@ class PackagesHandlersTest(unittest.TestCase):
         self.testbed.init_memcache_stub()
 
     def test_add_stream_handler(self):
-        request = webapp2.Request.blank('/streams/add/stream_id_1/stream_1')
+        request = webapp2.Request.blank('/streams')
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
-        streams = PackagesStream.query().fetch(2)
-        self.assertEqual(len(streams), 1)
-        self.assertEqual(streams[0].id, 'stream_id_1')
-        self.assertEqual(streams[0].name, 'stream_1')
 
     def test_add_package_handler(self):
         PackagesStream(id='stream_id_1', name='stream1', packages_id_list=[]).put()
-        request = webapp2.Request.blank('/streams/stream_id_1/packages/add/package_id_1/package1/1')
+        request = webapp2.Request.blank('/streams/stream_id_1/packages/add')
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
-        packages = PackageDictionary.query().fetch(2)
-        self.assertEqual(len(packages), 1)
-        self.assertEqual(packages[0].id, 'package_id_1')
-        self.assertEqual(packages[0].name, 'package1')
-        self.assertEqual(packages[0].release_time, 1)
 
     def test_change_words_handler(self):
         PackageDictionary(id='package_id_1', name='package1',
