6e38cf11c3eff8b369957d174f86863e391ded33
Ivan Pavlov <i.pavlov17@gmail.com>
2014-01-06 10:55:05 +0400

    added logout link

--- admin_page_handler.py
+++ admin_page_handler.py
@@ -8,4 +8,5 @@ class AdminPage(webapp2.RequestHandler):
         if not users.is_current_user_admin():
             self.redirect(users.create_login_url(self.request.uri))
         template = JINJA_ENVIRONMENT.get_template('templates/admin.html')
-        self.response.write(template.render())
+        self.response.write(template.render(
+            {"logout_link": users.create_logout_url('/')}))
