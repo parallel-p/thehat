da3ecb293fc1d3fd7c57016e3796e6981dbdb12e
Nikolay Zinov <nzinov@gmail.com>
2014-02-28 19:08:11 +0400

    fix

--- handlers/base_handlers/web_request_handler.py
+++ handlers/base_handlers/web_request_handler.py
@@ -17,7 +17,7 @@ class WebRequestHandler(GenericHandler):
             self.redirect(users.create_login_url())
         else:
             self.user_key = (User.query(User.user_id == self.user.user_id()).get(keys_only=True) or
-                             User(user_id=self.user.user_id(), user_object=self.user)).put()
+                             User(user_id=self.user.user_id(), user_object=self.user).put())
             webapp2.RequestHandler.dispatch(self)
 
     def __init__(self, *args, **kwargs):
