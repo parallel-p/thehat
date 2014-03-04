84ff3a296d8fe50445cfed13b093e5e47fdcec91
Pavlov Ivan <i.pavlov17@gmail.com>
2014-03-04 16:07:36 +0400

    added some trash

---
+++ handlers/statistics/word_statistics_handler.py
@@ -0,0 +1,15 @@
+__author__ = 'ivan'
+
+
+from environment import JINJA_ENVIRONMENT
+from handlers.base_handlers.web_request_handler import WebRequestHandler
+
+
+class WordStatisticsHandler(WebRequestHandler):
+
+    def __init__(self, *args, **kwargs):
+        super(WordStatisticsHandler, self).__init__(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        #template = JINJA_ENVIRONMENT.get_template('templates/statistics/word_statistic.html')
+        self.response.write("")
