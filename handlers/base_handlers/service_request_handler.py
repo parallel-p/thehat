c8da2933ad886c5876284b410802e0cf5e4b703b
Denis Kuznetsov <denspb@gmail.com>
2014-03-13 19:57:05 +0400

    make service requests permitted only for administrators

--- handlers/base_handlers/service_request_handler.py
+++ handlers/base_handlers/service_request_handler.py
@@ -1,9 +1,15 @@
 __author__ = 'ivan'
 
 from generic_handler import GenericHandler
+from google.appengine.api import users
 
 
 class ServiceRequestHandler(GenericHandler):
     def __init__(self, *args, **kwargs):
         super(ServiceRequestHandler, self).__init__(*args, **kwargs)
+
+    def dispatch(self):
+        if not users.is_current_user_admin():
+            self.abort(403)
+        super(ServiceRequestHandler, self).dispatch()
 
