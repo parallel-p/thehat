9448c51ae8f0bba950c90563cc034a90b8691b1d
Nikolay Zinov <nzinov@gmail.com>
2014-03-13 19:44:55 +0400

    improve our template system

--- handlers/base_handlers/web_request_handler.py
+++ handlers/base_handlers/web_request_handler.py
@@ -4,7 +4,9 @@ from google.appengine.api import users
 
 from generic_handler import GenericHandler
 from objects.user_devices import User
+
 import webapp2
+from environment import JINJA_ENVIRONMENT
 
 
 class WebRequestHandler(GenericHandler):
@@ -19,6 +21,14 @@ class WebRequestHandler(GenericHandler):
             self.user_key = (User.query(User.user_id == self.user.user_id()).get(keys_only=True) or
                              User(user_id=self.user.user_id(), user_object=self.user).put())
             webapp2.RequestHandler.dispatch(self)
+
+    def draw_page(self, template_name, **render_data):
+        template = JINJA_ENVIRONMENT.get_template('templates/{}.html'.format(template_name))
+        render_data['logout_link'] = users.create_logout_url('/')
+        render_data['user_email'] = users.get_current_user().email()
+        self.response.write(template.render(render_data))
+
+
 
     def __init__(self, *args, **kwargs):
         super(WebRequestHandler, self).__init__(*args, **kwargs)
