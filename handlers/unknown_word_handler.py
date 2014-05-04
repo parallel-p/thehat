49e757c9c0723ca9ec4855d52901a8c157f842af
Nikolay Zinov <nzinov@gmail.com>
2014-05-04 16:44:18 +0400

    refactor unknown words interface

--- handlers/unknown_word_handler.py
+++ handlers/unknown_word_handler.py
@@ -7,49 +7,30 @@ __author__ = 'ivan'
 from objects.unknown_word import UnknownWord
 
 
-class page_word():
-
-    def __init__(self, x, cnt):
-        self.times_used = x.times_used
-        self.ignored = x.ignored
-        self.word = x.word
-        self.id = cnt
-
-
 class GetWordPageHandler(AdminRequestHandler):
-
-    def __init(self, *args, **kwargs):
-        super(GetWordPageHandler, self).__init__(*args, **kwargs)
-
     def get(self):
         self.response.headers.add_header('Access-Control-Allow-Origin', '*')
-        words = [page_word(value, index) for index, value in enumerate(UnknownWord.query().order(-UnknownWord.times_used).filter(UnknownWord.ignored==False).fetch())]
-        self.draw_page('unknown_word_page', word_list=words, quantity=len(words))
+        words = UnknownWord.query().order(-UnknownWord.times_used).filter(UnknownWord.ignored == False).fetch()
+        self.draw_page('unknown_word_page', word_list=words)
+
+
+def ignore_word(word):
+    in_base = ndb.Key(UnknownWord, word).get()
+    in_base.ignored = True
+    in_base.put()
 
 
 class IgnoreWordHanler(AdminRequestHandler):
-
-    def __init__(self, *args, **kwargs):
-        super(IgnoreWordHanler, self).__init__(*args, **kwargs)
-
     def post(self):
         word = self.request.get("word")
-        in_base = ndb.Key(UnknownWord, word).get()
-        in_base.ignored = True
-        in_base.put()
+        ignore_word(word)
 
 
 class AddWordHanler(AdminRequestHandler):
-
-    def __init__(self, *args, **kwargs):
-        super(AddWordHanler, self).__init__(*args, **kwargs)
-
     def post(self):
         word = self.request.get("word")
         GlobalDictionaryWord(word=word, cnt=0, tags="", id=word).put()
-        key = ndb.Key(UnknownWord, word).get()
-        if key is not None:
-            key.key.delete()
+        ignore_word(word)
 
 
 
