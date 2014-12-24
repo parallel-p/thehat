c407a3ca529f52c6749f5aa7832fec0a47415da2
Nikolay Zinov <nzinov@gmail.com>
2014-12-24 09:38:43 +0300

    improve deferred_map

--- handlers/service/process.py
+++ handlers/service/process.py
@@ -3,12 +3,11 @@ from google.appengine.ext import ndb
 
 BATCH_SIZE = 100
 
-def _map(model_kind, function, cursor=None):
-    data, curs, more = ndb.Query(kind=model_kind).fetch_page(BATCH_SIZE, start_cursor=cursor)
+def deferred_map(model_kind, function, cursor=None, keys_only=False, write=False):
+    data, curs, more = ndb.Query(kind=model_kind).fetch_page(BATCH_SIZE, start_cursor=cursor, keys_only=keys_only)
     for entity in data:
         function(entity)
+    if write:
+        ndb.put_multi(data)
     if len(data) > 0 and more and curs:
-        deferred.defer(_map, model_kind, function, curs)
-
-def deferred_map(model_kind, function):
-    deferred.defer(_map, model_kind, function)
+        deferred.defer(deferred_map, model_kind, function, curs, keys_only, write)
